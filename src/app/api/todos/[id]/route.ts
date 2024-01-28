d7d1c2d8d1e59782ef1d8bb45d114d2b6ffa98f9
Kevin <keviinnalvarez2015@gmail.com>
2024-01-28 13:29:40 -0400

    validations in update & optimization code

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Todo } from "@prisma/client";
 import prisma from "@/lib/prisma";
+import * as yup from 'yup';
 
 interface Params {
     params: {
@@ -7,38 +9,48 @@ interface Params {
     }
 }
 
+const getTodo = async (id: string):Promise<Todo | null> => {
+    const todo = await prisma.todo.findUnique({ where: { id }});
+    return todo;
+}
+
 export async function GET(request: NextRequest, { params }: Params) {
-    const { id } = params;
-    const todo = await prisma.todo.findUnique({
-        where: { id }
-    });
+
+    const todo = await getTodo(params.id);
+    
     if (!todo) {
         return NextResponse.json(
-            { sms: `ID '${id}' not exist` },
+            { sms: `ID '${params.id}' not exist` },
             { status: 404 }
         )
     }
     return NextResponse.json(todo);
 }
 
+const putSchema = yup.object({
+    complete: yup.boolean().optional(),
+    description: yup.string().optional()
+});
+
 export async function PUT(request: NextRequest, { params }: Params) {
-    const { id } = params;
-    const todo = await prisma.todo.findUnique({
-        where: { id }
-    });
+
+    const todo = await getTodo(params.id);
     
     if (!todo) {
         return NextResponse.json(
-            { sms: `ID '${id}' not exist` },
+            { sms: `ID '${params.id}' not exist` },
             { status: 404 }
         )
     }
 
-    const body = await request.json();
-    const updateTodo = await prisma.todo.update({
-        where: { id },
-        data: {...body}
-    });
-
-    return NextResponse.json(updateTodo);
+    try {
+        const { complete, description } = await putSchema.validate(await request.json());
+        const updateTodo = await prisma.todo.update({
+            where: { id: params.id },
+            data: { complete, description }
+        });
+        return NextResponse.json(updateTodo);
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 }
