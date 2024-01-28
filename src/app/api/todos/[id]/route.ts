873810d69ac929985084f14b041a19c42ed15ee3
Kevin <keviinnalvarez2015@gmail.com>
2024-01-28 12:48:41 -0400

    endpoint for update todo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -20,3 +20,25 @@ export async function GET(request: NextRequest, { params }: Params) {
     }
     return NextResponse.json(todo);
 }
+
+export async function PUT(request: NextRequest, { params }: Params) {
+    const { id } = params;
+    const todo = await prisma.todo.findUnique({
+        where: { id }
+    });
+    
+    if (!todo) {
+        return NextResponse.json(
+            { sms: `ID '${id}' not exist` },
+            { status: 404 }
+        )
+    }
+
+    const body = await request.json();
+    const updateTodo = await prisma.todo.update({
+        where: { id },
+        data: {...body}
+    });
+
+    return NextResponse.json(updateTodo);
+}
