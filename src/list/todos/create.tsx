632721fd9ae266339f3e2592a193de4485b2bfc5
Kevin <keviinnalvarez2015@gmail.com>
2024-01-29 10:44:19 -0400

    update styles

--- src/list/todos/create.tsx
+++ src/list/todos/create.tsx
@@ -3,12 +3,12 @@ import { IoTrashOutline } from 'react-icons/io5';
 
 export const CreateTodo = () => {
     return (
-        <form className='flex'>
+        <form className='flex gap-2 items-center'>
             <input type='text'
                 className='w-6/12 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all'
                 placeholder='¿Cuál es la nueva tarea?' />
 
-            <button type='submit' className='flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all'>
+            <button type='submit' className='flex items-center justify-center rounded h-10 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all'>
                 Crear
             </button>
 
@@ -16,7 +16,7 @@ export const CreateTodo = () => {
 
             <button
                 //TODO: onClick={ () => deleteCompleted() }
-                type='button' className='flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all'>
+                type='button' className='flex items-center justify-center rounded h-10 bg-red-400 p-2 text-white hover:bg-red-700 transition-all'>
                 <IoTrashOutline />
                 Delete
             </button>
