3f2cca8f4ebaecae2ef58b1ff1ec4879c5774ee7
Kevin <keviinnalvarez2015@gmail.com>
2024-02-01 13:42:57 -0400

    use hook optimistic

--- src/list/todos/items.tsx
+++ src/list/todos/items.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { startTransition, useOptimistic } from 'react'
 import { Todo } from '@prisma/client'
 import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5'
 import styles from './item.module.css'
@@ -9,25 +10,39 @@ interface Props {
 }
 
 export const TodoItem = ({ todo, onTodo }: Props) => {
-    const { id, description, complete } = todo;
+    // Simulate hook useState
+    const [todoOptimized, setTodoOptimized] = useOptimistic(
+        todo,
+        (state, newComplete: boolean) => ({...state, newComplete })
+    );
+
+    const onToggleTodo = async () => {
+        try {
+            startTransition(() => setTodoOptimized(!todoOptimized.complete));
+            await onTodo(todoOptimized.id, !todoOptimized.complete)
+        } catch (error) {
+            startTransition(() => setTodoOptimized(!todoOptimized.complete));
+        }
+    }
 
   return (
-      <div className={complete ? styles.todoDone : styles.todoPending}>
+      <div className={todoOptimized.complete ? styles.todoDone : styles.todoPending}>
         <div className='flex flex-col sm:flex-row justify-start items-center gap-5'>
             <div
-                onClick={() => onTodo(id, !complete)}
+                // onClick={() => onTodo(todoOptimized.id, !todoOptimized.complete)}
+                onClick={onToggleTodo}
                 className={`
                 flex p-2 rounded-md cursor-pointer
                 hover:bg-opacity-60
-               ${complete ? ' bg-blue-100' : ' bg-red-100'}
+               ${todoOptimized.complete ? ' bg-blue-100' : ' bg-red-100'}
             `}>
-                {complete
+                  {todoOptimized.complete
                     ? <IoCheckboxOutline size={30} />
                     : <IoSquareOutline size={30} />
                 }
             </div>
             <div className='text-center sm:text-left'>
-                {description}
+                {todoOptimized.description}
             </div>
         </div>
     </div>
