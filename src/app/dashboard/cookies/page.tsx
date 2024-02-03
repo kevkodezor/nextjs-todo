3eed39fedbaabc6b8dfef4f3428fc97a4e4e6b88
Kevin <keviinnalvarez2015@gmail.com>
2024-02-03 17:08:52 -0400

    get cookie in Server component

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import { TabBar } from '@/components';
 
 export const metadata = {
@@ -6,10 +7,13 @@ export const metadata = {
 
 export default function Cookies() {
 
+    const cookieStore = cookies();
+    const cookieTab = cookieStore.get('selectedTab')?.value ?? '1';
+
     return (
         <div className='flex flex-col gap-3 bg-white rounded-md p-2 h-96'>
             <span className='text-3xl'>TabBar</span>
-            <TabBar />
+            <TabBar indexTab={Number(cookieTab)} />
         </div>
     )
 }
