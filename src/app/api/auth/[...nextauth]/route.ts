a33dc4c64f1def66811b513f7f7f92c409ac787c
Kevin <keviinnalvarez2015@gmail.com>
2024-02-12 16:41:13 -0400

    Prima adapter for connection with provider login & new dependencies

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
+import prisma from '@/lib/prisma';
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { Adapter } from 'next-auth/adapters';
+
 
 export const authOptions:NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? '',
