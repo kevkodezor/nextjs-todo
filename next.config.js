465e425c03d8ce20d52bbf46d5125332aae2a877
Kevin <keviinnalvarez2015@gmail.com>
2024-02-12 12:29:12 -0400

    new permission hostname

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             { protocol: 'https', hostname: 'tailus.io' },
+            { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
         ]
     }
 }
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,6 +4,8 @@ import { CiLogout, CiHome, CiSquareCheck, CiBoxList } from 'react-icons/ci';
 import { PiCookieDuotone } from 'react-icons/pi';
 import { SideItems } from './sideitems';
 import { IoCartOutline } from 'react-icons/io5';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const menuItems = [
     { icon: <CiHome />, title: 'Home', route: '/dashboard' },
@@ -13,7 +15,14 @@ const menuItems = [
     { icon: <IoCartOutline />, title: 'Products', route: '/dashboard/products' },
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+
+    const session = await getServerSession(authOptions);
+
+    const username = session?.user?.name ?? 'User name';
+    const avatar = session?.user?.image ?? 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+    // const role = session?.user?.role ?? 'User role';
+
     return (
         <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
             <div>
@@ -31,14 +40,14 @@ export const Sidebar = () => {
 
                 <div className="mt-8 text-center">
                     <Image
-                        src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
-                        alt=""
+                        src={avatar}
+                        alt='photo profile'
                         className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
                         width={150}
                         height={150}
                     />
-                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">The admin</h5>
-                    <span className="hidden text-gray-400 lg:block">Admin</span>
+                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{username}</h5>
+                    <span className="hidden text-gray-400 lg:block">'Admin</span>
                 </div>
 
                 <ul key='8991' className='space-y-2 tracking-wide mt-8 overflow-auto'>
