411309cb3febb7e3ec93719d0ca90a5c852b89b1
Kevin <keviinnalvarez2015@gmail.com>
2024-02-16 12:07:10 -0400

    Credentials provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import prisma from '@/lib/prisma';
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { Adapter } from 'next-auth/adapters';
 
@@ -16,6 +17,27 @@ export const authOptions:NextAuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID ?? '',
             clientSecret: process.env.GITHUB_SECRET ?? '',
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: 'Email', type: 'email', placeholder: 'enter your email' },
+                password: { label: 'Password', type: 'password', placeholder: 'enter your password' }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
         }),
     ],
     session: {
